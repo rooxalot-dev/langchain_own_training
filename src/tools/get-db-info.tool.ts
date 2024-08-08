0fc07a4c21cffb8a351330cf1465451e2febb916
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-08-08 16:01:20 -0300

    WIP: Enable agent to query the configured database to retrive info

--- src/tools/get-db-info.tool.ts
+++ src/tools/get-db-info.tool.ts
@@ -4,7 +4,7 @@ import { openAIFactory } from "../factory/implementation/openai/openai";
 import { getPostgresDatasource } from "../db/postgres/sql.database";
 
 export const getDbInfoTools = async () => {
-  const model = openAIFactory({ verbose: true });
+  const model = openAIFactory();
   const dataSource = await getPostgresDatasource();
   const sqlToolKit = new SqlToolkit(dataSource, model);
 
